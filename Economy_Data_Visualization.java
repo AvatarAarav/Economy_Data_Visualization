6d00eb682a53b6184de911f1edcd775effa67b7c
Aarav Nigam <aarav.n21@iiits.in>
2022-10-01 13:30:26 +0530

    Added code

--- Economy_Data_Visualization.java
+++ Economy_Data_Visualization.java
@@ -2,5 +2,6 @@ public class Economy_Data_Visualization {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Let's make a nice project");
+        System.out.println("Hey");
     }
 }
