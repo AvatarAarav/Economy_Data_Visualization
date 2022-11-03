51443b478ef27057542af96cbec700f1b700bf05
Aarav Nigam <aarav.n21@iiits.in>
2022-11-03 17:28:49 +0530

    Adding jfreeChart jar file

--- Economy_Data_Visualization.java
+++ Economy_Data_Visualization.java
@@ -2,7 +2,6 @@ import Development_Indicators.*;
 import Assets_Indicators.*;
 import static java.lang.System.*;
 import java.util.Scanner;
-
 public class Economy_Data_Visualization {
 
     public static void main(String[] args) {
