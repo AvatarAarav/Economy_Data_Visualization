6b5bc6ad37d43d44449cede167f1f1ce00f1d71c
PratSins <pratyush2002ps@gmail.com>
2022-11-09 11:35:38 +0530

    adding db functionalities

--- Economy_Data_Visualization.java
+++ Economy_Data_Visualization.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Economy_Data_Visualization {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc=new Scanner(in);
     }
 }
