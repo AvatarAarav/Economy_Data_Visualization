d4616a1570ab9c7b9c0a1fe0d0c69c7daf48e693
AvatarAarav <aarav.n21@iiits.in>
2022-11-17 15:36:23 +0530

    Adding UI

--- SQL_Acc.java
+++ SQL_Acc.java
@@ -3,7 +3,7 @@ public class SQL_Acc
     final String url = "jdbc:mysql://localhost:3306/";
 
     final String username = "root";
-    final String pwd = "root12345";
+    final String pwd = "28062004";
 
     final String dbName = "economicdata";
 }
