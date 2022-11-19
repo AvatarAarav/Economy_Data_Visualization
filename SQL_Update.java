75dd5d64dbaccbefc7e94213058ac87ef07894c7
PratSins <pratyush2002ps@gmail.com>
2022-11-19 16:31:17 +0530

    SQL_Update ADDED

--- SQL_Update.java
+++ SQL_Update.java
@@ -1,31 +1,30 @@
 import java.sql.*;
-import java.io.*;
 import static java.lang.System.*;
 public class SQL_Update
 {
-    public static void main(String[] args)throws Exception
+    static SQL_Acc acc = new SQL_Acc();
+    static String turl = acc.url + acc.dbName;
+    static Connection c = ConnectionProvider.getConnection(turl, acc.username, acc.pwd);
+
+    static void change(String tN, String yr, String cc, double val)
     {
-        String url = "jdbc:mysql://localhost:3306/myjdbc";
-        String un = "root", pwd = "root12345";
-        Connection c = ConnectionProvider.getConnection(url,un,pwd);
+        try {
+            String q = "update " + tN + " set Y" + yr + "=? where Country_Code=?";
 
-        String q = "update table1 set tName=?, tCity=? where tID=?";
+            PreparedStatement pmt = c.prepareStatement(q);
+            pmt.setDouble(1, val);
+            pmt.setString(2, cc);
+            pmt.executeUpdate();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        out.println("Enter new Name and City:");
-        String n = br.readLine();
-        String city = br.readLine();
-
-        out.print("Enter ID: ");
-        int id = Integer.parseInt(br.readLine());
-
-        PreparedStatement pmt = c.prepareStatement(q);
-        pmt.setString(1,n);
-        pmt.setString(2,city);
-        pmt.setInt(3,id);
-
-        pmt.executeUpdate();
-
-        out.println("Job done..");
+            out.println("Table Updated Successfully...");
+        }
+        catch(Exception e){
+            out.println("Table Update failed!!!");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args)
+    {
+        //change("consumer_price_index","1960","AFG",1964.1);
     }
 }
